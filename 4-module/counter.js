539799b41dec90333fb5e2eb69c1712644fcfb71
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-25 15:20:24 +0900

    Nodejs : module
    exports, require

---
+++ 4-module/counter.js
@@ -0,0 +1,16 @@
+//not exported to outside
+let count=0;
+
+function increase(){
+    count++;
+}
+
+function getCount(){
+    return count;
+}
+
+//exports
+module.exports.getCount=getCount;
+module.exports.increase=increase;
+
+console.log(module);
