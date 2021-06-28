7256889f699d45b1d62bec221bf3fbf0008d8e1f
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-28 15:52:15 +0900

    Nodejs : express
    concept of middleware

--- exp/app.js
+++ exp/app.js
@@ -1,14 +1,35 @@
 import express from 'express'
 const app=express();
 
-//Port
-app.get('/sky/:id',(req,res,next) => {
-    //console.log(req.path);
-    //console.log(req.headers);
-    console.log(req.params.id); //localhost:8080/sky/soomin => soomin
-    console.log(req.query.keyword); //http://localhost:8080/sky/soomin/?keyword=study
-    //res.json({name: 'soomin'});
-    res.setHeader('key','value');
-    res.status(201).send('created!');
+app.all('/api',(req,res,next) => {
+    console.log('all');
+    next();
 });
+app.use('/sky',(res,req,next) => {
+    console.log('use');
+    next();
+});
+app.get('/',
+    (req,res,next) =>{
+    console.log('first');
+    next();
+    },
+    (req,res,next) =>{
+    console.log('first-1');
+    next();
+    }
+);
+
+app.get('/',(req,res,next) =>{
+    console.log('second');
+    res.send('The End!');
+});
+
+app.use((req,res,next) => {
+    res.status(404).send('Not found!');
+})
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send('sorry');
+})
 app.listen(8080);
