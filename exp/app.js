9855564d0402560626037ce355035248e381cc84
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-28 15:24:24 +0900

    Nodejs : express
    params, query 에 대해 알아봄

--- exp/app.js
+++ exp/app.js
@@ -2,8 +2,11 @@ import express from 'express'
 const app=express();
 
 //Port
-app.get('/',(req,res,next) => {
-    console.log('get');
+app.get('/sky/:id',(req,res,next) => {
+    //console.log(req.path);
+    //console.log(req.headers);
+    console.log(req.params.id); //localhost:8080/sky/soomin => soomin
+    console.log(req.query.keyword); //http://localhost:8080/sky/soomin/?keyword=study
     res.send('hi');
 });
 app.listen(8080);
