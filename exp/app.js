54c2abfc74daf9ccc05c2b09f05fef7207ded4bd
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-28 15:31:51 +0900

    Nodejs : express(res)
    response APIs

--- exp/app.js
+++ exp/app.js
@@ -7,6 +7,8 @@ app.get('/sky/:id',(req,res,next) => {
     //console.log(req.headers);
     console.log(req.params.id); //localhost:8080/sky/soomin => soomin
     console.log(req.query.keyword); //http://localhost:8080/sky/soomin/?keyword=study
-    res.send('hi');
+    //res.json({name: 'soomin'});
+    res.setHeader('key','value');
+    res.status(201).send('created!');
 });
 app.listen(8080);
