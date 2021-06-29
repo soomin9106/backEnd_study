ebaf8c2787e36cce6c83b6e19e1e357b674163d2
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-06-29 09:40:36 +0900

    Nodejs :express(post)
    POST 를 express 에서 처리

--- exp/app.js
+++ exp/app.js
@@ -1,35 +1,12 @@
 import express from 'express'
 const app=express();
 
-app.all('/api',(req,res,next) => {
-    console.log('all');
-    next();
-});
-app.use('/sky',(res,req,next) => {
-    console.log('use');
-    next();
-});
-app.get('/',
-    (req,res,next) =>{
-    console.log('first');
-    next();
-    },
-    (req,res,next) =>{
-    console.log('first-1');
-    next();
-    }
-);
+//post 처리
+app.use(express.json());
 
-app.get('/',(req,res,next) =>{
-    console.log('second');
-    res.send('The End!');
+app.post('/',(req,res,next) =>{
+    console.log(req.body);
 });
 
-app.use((req,res,next) => {
-    res.status(404).send('Not found!');
-})
-app.use((error, req, res, next) => {
-    console.error(error);
-    res.status(500).send('sorry');
-})
+
 app.listen(8080);
