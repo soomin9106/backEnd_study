82b9dd6058b3c3b5ec74adc38dbd51316a6c88e1
soomin9106 <79344555+soomin9106@users.noreply.github.com>
2021-07-01 14:41:47 +0900

    Nodejs : express (middleware)

--- 7-cors/app.js
+++ 7-cors/app.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import cors from 'cors';
+import cookieParser from 'cookie-parser'; // cookie 확인
+import morgan from 'morgan'; //어떤 요청이 얼마나 걸렸는지 
+import helmet from 'helmet';
 
 const app=express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('combined'));
+
 app.use(
     cors({
         origin: ['http://127.0.0.1:5500'],
@@ -9,8 +16,11 @@ app.use(
         credentials: true,
     })
 );
+app.use(helmet()); //보안에 관련된 header 추가
 
 app.get('/',(req,res) => {
+    console.log(req.body);
+    console.log(req.cookies);
     res.send("Welcome!");
 });
 
